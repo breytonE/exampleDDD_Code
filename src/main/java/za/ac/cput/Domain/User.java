0d73d3cb4935b84fda99bd146d8a0bd4c94c2e8f
breySean <breytonernstzen@gmail.com>
2022-03-04 19:28:02 +0200

    user class implemented with builder pattern

--- src/main/java/za/ac/cput/Domain/User.java
+++ src/main/java/za/ac/cput/Domain/User.java
@@ -1,4 +1,75 @@
 package za.ac.cput.Domain;
 
 public class User {
+    private String firstName;
+    private String lastName;
+    private String email;
+
+    private User(Builder b) {
+        this.firstName = b.firstName;
+        this.lastName = b.lastName;
+        this.email = b.email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void display() {
+        System.out.println("User first name: " + this.firstName);
+        System.out.println("User last name: " + this.lastName);
+        System.out.println("User email: " + this.email);
+    }
+
+    public static class Builder {
+        private String firstName;
+        private String lastName;
+        private String email;
+
+        public Builder setFirstName(String firstName) {
+            this.firstName = firstName;
+            return this;
+        }
+
+        public Builder setLastName(String lastName) {
+            this.lastName = lastName;
+            return this;
+        }
+
+        public Builder setEmail(String email) {
+            this.email = email;
+            return this;
+        }
+
+        public Builder copy(User user) {
+            this.firstName = user.firstName;
+            this.lastName = user.lastName;
+            this.email = user.email;
+            return this;
+        }
+
+        public User build() {
+            return new User(this);
+        }
+    }
 }
